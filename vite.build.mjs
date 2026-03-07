9840846399df5c75cd8ada09e9ed6ee999bb7fa8
Robbie Calvin <robert.calvin.dev@gmail.com>
2026-03-07 11:46:37 -0800

    red-door: point pages frontend to render api endpoints

--- vite.build.mjs
+++ vite.build.mjs
@@ -2,6 +2,8 @@ import react from "@vitejs/plugin-react";
 import { build } from "vite";
 
 async function main() {
+  const apiBasePath = process.env.DUALMODE_API_BASE_PATH ?? "https://red-door-api.onrender.com";
+  const wsUrl = process.env.DUALMODE_WS_URL ?? "wss://red-door-api.onrender.com/ws";
   await build({
     configFile: false,
     clearScreen: false,
@@ -10,8 +12,8 @@ async function main() {
     define: {
       __DUALMODE_DEFAULT_CENTER_LAT__: JSON.stringify(process.env.DUALMODE_DEFAULT_CENTER_LAT ?? "0"),
       __DUALMODE_DEFAULT_CENTER_LNG__: JSON.stringify(process.env.DUALMODE_DEFAULT_CENTER_LNG ?? "0"),
-      __DUALMODE_API_BASE_PATH__: JSON.stringify(process.env.DUALMODE_API_BASE_PATH ?? "__local__"),
-      __DUALMODE_WS_URL__: JSON.stringify(process.env.DUALMODE_WS_URL ?? "__disabled__"),
+      __DUALMODE_API_BASE_PATH__: JSON.stringify(apiBasePath),
+      __DUALMODE_WS_URL__: JSON.stringify(wsUrl),
     },
     plugins: [react()],
     base: "./",
